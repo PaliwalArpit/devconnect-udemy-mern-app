d0c04986ef8a0140a99dd001d052edd954b3ab4a
Arpit Paliwal <apaliwal@localhost.localdomain>
2021-06-05 19:36:22 +0530

    added Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import React, { Fragment } from 'react';
-import Navbar from './Components/Layout/Navbar';
-import Landing from './Components/Layout/Landing';
-const  App = () => {
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Fragment } from "react";
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import Navbar from "./Components/Layout/Navbar";
+import Landing from "./Components/Layout/Landing";
+const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
